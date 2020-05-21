afd28253820779404cf436ce570c38d38df6432f
achand <achand@uno.edu>
2020-05-21 00:51:45 -0500

    changed db

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'M@nchester13',
-    database: 'jobdb',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 const register = require('./controllers/register');
